3c5ce6a25ed6eee1e097c738e429f1e6cfe9117f
Konrad Poznanski <konrad.poznanski@gmail.com>
2023-06-26 01:12:22 +0200

    test

--- sources/ChaosMonkeyPod/src/test/java/it/poznanski/chaosmonekypod/test/RestAPITest.java
+++ sources/ChaosMonkeyPod/src/test/java/it/poznanski/chaosmonekypod/test/RestAPITest.java
@@ -11,27 +11,40 @@ import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
 import it.poznanski.chaosmonkeypod.rest.client.K8SRestClient;
 import it.poznanski.chaosmonkeypod.rest.client.K8SRestClientException;
 import it.poznanski.chaosmonkeypod.utils.Properties;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class RestAPITest {
 
-    /*private KubernetesServer server = new KubernetesServer();
-    private KubernetesClient fabric8Client = server.getClient();
-    private String token = fabric8Client.getConfiguration().getOauthToken();
-    @Before
-    public void initTest(){
-        server.expect().get().withPath("/api/v1/namespaces/testing/pods?labelSelector=chaos-monkey%3Dmonkey-victim").andReturn(HttpURLConnection.HTTP_OK, )
+    private KubernetesServer server;
+    private KubernetesClient fabric8Client;
 
+    private String token;
+    @Before
+    public void initTest() throws IOException{
+        Path path = Paths.get("src/test/resources/podList.json");
+        List<String> lines = Files.readAllLines(path);
+        StringBuffer buffer = new StringBuffer();
+        for(String line:lines){
+            buffer.append(line);
+        }
+        String podListText = buffer.toString();
+        JSONObject jsonObject = new JSONObject(podListText);
+        this.server = new KubernetesServer();
+        this.server.expect().get().withPath("/api/v1/namespaces/testing/pods?labelSelector=chaos-monkey%3Dmonkey-victim").andReturn(HttpURLConnection.HTTP_OK, jsonObject).once();
+        this.fabric8Client = server.getClient();
+        this.token = fabric8Client.getConfiguration().getOauthToken();
     }
     @Test
-    @KubernetesTest
-    @LoadKubernetesManifests("target-deployment.yaml")
     public void listPodsWithLabelTest() throws IOException, K8SRestClientException {
         Properties properties = Properties.getInstance();
         String namespace = properties.getProperties().getProperty("target.namespace");
@@ -39,5 +52,5 @@ public class RestAPITest {
         K8SRestClient client = new K8SRestClient(this.token);
         List pods = client.getPodsWithLabel(namespace, label);
         assert(pods.size() != 0);
-    }*/
+    }
 }
